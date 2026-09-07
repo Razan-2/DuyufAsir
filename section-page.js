9b4c2dd73de112c9cef08e751b6cdf1fb357d052
Razan-2 <razanalqobti@gmail.com>
2026-09-07 03:12:20 +0300

    Use Asir sunset scene for homepage hero

--- section-page.js
+++ section-page.js
@@ -154,7 +154,7 @@ fetch("/")
 
         const script = document.createElement("script");
 
-        script.src = "/main.js?v=263";
+        script.src = "/main.js?v=264";
         script.defer = true;
 
         document.body.appendChild(script);
